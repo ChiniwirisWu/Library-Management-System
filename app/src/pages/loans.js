6751704ec6ae1d935e5f2c0200745b87fe7767f9
DanielRamirez404 <danielramirezabou404@gmail.com>
2024-11-13 20:33:22 -0400

    reworked the loans tab's components

--- app/src/pages/loans.js
+++ app/src/pages/loans.js
@@ -2,73 +2,96 @@ import React from "react";
 import { MainPage } from "./components/reusables";
 import { GetPathTitle } from "./constants/pages";
 import { PagePaths } from "./constants/paths";
+import { Entry } from "./components/reusables";
+import { limitString } from "../functions/strings";
+import { IconLink } from "./components/reusables";
+import { IconButton } from "./components/reusables";
+import AcceptIcon from "res/accept.svg";
+import DenyIcon from "res/deny.svg";
+import InfoImage from "res/info.svg"
 
-function Content() {
+
+function LoanEntryInfo({title, reader, phone, days, address}) {
+    const size = 50;
+
     return (
-        <div className="text-xl flex flex-col h-[100%] w-[100%] items-center content-center  text-center">
-            <div class="w-3/4 min-h-[125px] py-5 px-10 bg-gray-50 border-solid border-[1px] border-gray rounded-sm mt-10 flex items-center justify-between ">
-                <div class="flex flex-col">
-                    <p class="text-lg font-bold">(Titulo del libro)</p>
-                    <p class="text-base">12.345.678 • Josefina Lopez • 0414-1234567 • Bna</p>
-                </div>
-                <div class="flex space-x-5">
-                    <button class="w-[95px] p-2 border-2 border-solid border-[#00AC4E] rounded-md text-[#00AC4E] hover:text-white hover:bg-[#00AC4E] transition-all">Aceptar</button>
-                    <button class="w-[95px] p-2 border-2 border-solid border-red-500 rounded-md text-red-500 hover:text-white hover:bg-red-500 transition-all">Rechazar</button>
-                </div>
-            </div>
-            <div class="w-3/4 min-h-[125px] py-5 px-10 bg-gray-50 border-solid border-[1px] border-gray rounded-sm mt-5 flex items-center justify-between ">
-                <div class="flex flex-col">
-                    <p class="text-lg font-bold">(Titulo del libro)</p>
-                    <p class="text-base">12.345.678 • Alfonso Guzman • 0414-1234567 • Pto</p>
-                </div>
-                <div class="flex space-x-5">
-                    <button class="w-[95px] p-2 border-2 border-solid border-[#00AC4E] rounded-md text-[#00AC4E] hover:text-white hover:bg-[#00AC4E] transition-all">Aceptar</button>
-                    <button class="w-[95px] p-2 border-2 border-solid border-red-500 rounded-md text-red-500 hover:text-white hover:bg-red-500 transition-all">Rechazar</button>
-                </div>
-            </div>
+        <div className="flex flex-col align-middle text-center">
+            <h6 className="font-bold text-xl">{limitString(title, size)}</h6>
+            <h6 className="font-bold text-sm">{limitString(reader, size)}</h6>
 
-            <table className="min-w-full bg-white border border-gray-200 ">
+            <p className="text-gray-600 font-light text-sm">{limitString(days.toString().concat(' días • ').concat(phone).concat(' • ').concat(address), size)}</p>
+        </div>
+    );
+}
+
+function LoanEntryIcons() {
+    return (
+        <>
+            <IconLink src={AcceptIcon} alt="accept" path={ PagePaths['Ficha'] } />
+            <IconLink src={DenyIcon} alt="deny" path={ PagePaths['Ficha'] } />
+            <IconLink src={InfoImage} alt="info" path={ PagePaths['Ficha'] } />
+        </>
+    );
+}
+
+function LoanEntry({title, reader, phone, days, address}) {
+    return (
+        <Entry 
+            info = <LoanEntryInfo title={title} reader={reader} phone={phone} days={days} address={address} />
+            icons = <LoanEntryIcons />
+        />
+    );
+}
+
+function LoansTable({data}) {
+
+    const th_style = "py-2 px-4 border-b border-gray-200 text-base font-semibold text-gray-700 text-center";
+    const td_style = "text-sm px-1 py-1 text-black text-center";
+
+    let columns = ["Título", "Prestatario", "Encargado", "Fecha", "Duración", "Cédula", "Teléfono", "Teléfono Vecino", "Dirección"];
+
+    return(
+        <table className="min-w-full bg-white border border-gray-200 ">
                 <thead className="bg-gray-100">
                     <tr>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">ISBN</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Fecha</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Duración</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Trabajador</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Cédula</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Nombre</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Apellido</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Dirección</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Teléfono</th>
-                        <th className="py-2 px-4 border-b border-gray-200 text-left text-sm font-semibold text-gray-700">Teléfono Vecino</th>
+                        { columns.map( (column) => ( <th className={th_style}>{column}</th>) ) }
+                        <th className={th_style}>Terminar</th>
                     </tr>
                 </thead>
                 <tbody>
+                    {
+                        data.map( (record) => (
+                            <tr className="hover:bg-gray-100 transition-all">
+                                { record.map( (field) => <td className={td_style}>{field}</td>) }
+                                <td className={td_style}>
+                                    <IconButton src={DenyIcon} alt="end" />
+                                </td>
+                            </tr>
+                        ))
+                    }
                     <tr className="hover:bg-gray-100 transition-all">
-                        <td className="py-2 px-4 border-b border-gray-200">978-3-16-148410-0</td>
-                        <td className="py-2 px-4 border-b border-gray-200">2023-10-01</td>
-                        <td className="py-2 px-4 border-b border-gray-200">30 días</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Juan Pérez</td>
-                        <td className="py-2 px-4 border-b border-gray-200">12345678</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Josefina</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Lopez</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Calle Falsa 123</td>
-                        <td className="py-2 px-4 border-b border-gray-200">0414-1234567</td>
-                        <td className="py-2 px-4 border-b border-gray-200">0414-7654321</td>
                     </tr>
-                    <tr className="hover:bg-gray-100 transition-all">
-                        <td className="py-2 px-4 border-b border-gray-200">978-1-23456-789-7</td>
-                        <td className="py-2 px-4 border-b border-gray-200">2023-10-02</td>
-                        <td className="py-2 px-4 border-b border-gray-200">15 días</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Maria Gomez</td>
-                        <td className="py-2 px-4 border-b border-gray-200">87654321</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Alfonso</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Guzman</td>
-                        <td className="py-2 px-4 border-b border-gray-200">Avenida Siempre Viva 742</td>
-                        <td className="py-2 px-4 border-b border-gray-200">0414-9876543</td>
-                        <td className="py-2 px-4 border-b border-gray-200">0414-3456789</td>
-                    </tr>
+                    
                 </tbody>
             </table>
+    );  
+}
+
+function Content() {
+
+    let data = [
+        ["El hobbit", "Marta Jiménez", "LuisAlb56", "19-11-23", "3 días", "43.235.761", "111222333", "2223331112", "Puerto la Cruz"],
+        ["Harry Potter y la piedra filosofal", "Luis Dominguez", "LuisAlb56", "29-10-24", "2 días", "89.111.223", "5555555555", "8888888888", "Barcelona"]
+    ];
+
+
+
+    return (
+        <div className="flex flex-col w-[75%] self-center pt-5">
+
+            <LoanEntry title="El Principito" reader="Gloria Velázquez" phone="1234567890" days={3} address="Barcelona" />
+            <LoansTable data={data}/>
+
         </div>
     );
 }
